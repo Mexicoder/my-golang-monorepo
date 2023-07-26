52bd731d7a928c4d41451b930099eb1f2bb64831
Mexicoder <superjohn140@gmail.com>
2023-07-26 11:19:17 -0400

    commit

--- my-other-module/go.mod
+++ my-other-module/go.mod
@@ -1,3 +1,7 @@
 module github.com/Mexicoder/my-golang-monorepo/my-other-module
 
 go 1.20
+
+require github.com/Mexicoder/my-golang-monorepo/linked-list v0.0.0-20230726145823-9b47e3aedbb5
+
+require github.com/Mexicoder/my-golang-monorepo/deeper-module/double-linked-list v0.0.0-20230726151753-d40d73f18c86 // indirect
--- my-other-module/uselist.go
+++ my-other-module/uselist.go
@@ -1,8 +1,16 @@
 package my_other_module
 
-import linked_list "github.com/Mexicoder/my-golang-monorepo/linked-list"
+import (
+	double_linked_list "github.com/Mexicoder/my-golang-monorepo/deeper-module/double-linked-list"
+	linked_list "github.com/Mexicoder/my-golang-monorepo/linked-list"
+)
 
 func main() {
 	list := linked_list.SingleLinkedList{}
 	_ = list
+	otherlist := double_linked_list.DoublyLinkedList{}
+	_ = otherlist
 }
+
+//git remote add origin https://github.com/Mexicoder/my-golang-monorepo.git
+//git push -u origin main
